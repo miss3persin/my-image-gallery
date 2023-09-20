b5065db2b809befa2821bc5d9000a3c967569457
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2023-09-20 12:32:08 +0100

    header component resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import Header from "./components/header";
+import Header from "./components/Header";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
